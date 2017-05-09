73ea7378bbcb2c83f89262ea26cf0c45529380fb
Claudio Capobianco <wbigger@gmail.com>
2017-05-09 18:53:44 +0200

    Move string from body to private static fields.

--- app/src/main/java/it/softlab/app_oco/utilities/JsonUtils.java
+++ app/src/main/java/it/softlab/app_oco/utilities/JsonUtils.java
@@ -12,35 +12,48 @@ import it.softlab.app_oco.model.Product;
 
 public class JsonUtils {
 
+    private static final String KEY_ERROR = "errorMessage";
+
+    private static final String KEY_OPERATION = "findItemsByKeywordsResponse";
+    private static final String KEY_SEARCH = "searchResult";
+    private static final String KEY_ITEM = "item";
+
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_LOCATION = "location";
+    private static final String KEY_SELLING_STATUS = "sellingStatus";
+    private static final String KEY_CURRENT_PRICE = "currentPrice";
+    private static final String KEY_VALUE = "__value__";
+    private static final String KEY_GLOBALID = "globalId";
+
     public static Product[] getProductDetails(String jsonString) throws JSONException {
         Product[] parsedData = null;
 
         JSONObject dataJson = new JSONObject(jsonString);
-        if (dataJson.has("errorMessage")) {
+        if (dataJson.has(KEY_ERROR)) {
             return null;
         }
 
-        JSONObject response = dataJson.getJSONArray("findItemsByKeywordsResponse")
+        JSONObject response = dataJson.getJSONArray(KEY_OPERATION)
                 .getJSONObject(0);
 
-        JSONObject searchResult = response.getJSONArray("searchResult")
+        JSONObject searchResult = response.getJSONArray(KEY_SEARCH)
                 .getJSONObject(0);
 
-        JSONArray itemArray = searchResult.getJSONArray("item");
+        JSONArray itemArray = searchResult.getJSONArray(KEY_ITEM);
 
         parsedData = new Product[itemArray.length()];
 
         for (int i = 0; i < itemArray.length(); i++) {
             JSONObject item = itemArray.getJSONObject(i);
 
-            String title = item.getJSONArray("title").getString(0);
-            String location = item.getJSONArray("location").getString(0);
-            String price = item.getJSONArray("sellingStatus")
+            String title = item.getJSONArray(KEY_TITLE).getString(0);
+            String location = item.getJSONArray(KEY_LOCATION).getString(0);
+            String price = item.getJSONArray(KEY_SELLING_STATUS)
                     .getJSONObject(0)
-                    .getJSONArray("currentPrice")
+                    .getJSONArray(KEY_CURRENT_PRICE)
                     .getJSONObject(0)
-                    .getString("__value__");
-            String country = item.getJSONArray("globalId").getString(0);
+                    .getString(KEY_VALUE);
+            String country = item.getJSONArray(KEY_GLOBALID).getString(0);
 
             parsedData[i] = new Product(title,location,price,country);
         }
